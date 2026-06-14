c6e93f2ff8a9f5a54dc8d51a2aea658932218f10
Tanisha-ML <scarlet82935@gmail.com>
2026-06-14 14:54:12 +0530

    added expense tracker project

---
+++ lesson1.py
@@ -0,0 +1,13 @@
+student={
+    "name": "John Doe",
+    "marks":82
+}
+if student["marks"]>=90:
+    print("Grade A")
+elif student["marks"]>=75:
+    print("Grade B") 
+elif student["marks"]>=60:
+    print("Grade C")
+else:
+    print("Grade D")
+          
